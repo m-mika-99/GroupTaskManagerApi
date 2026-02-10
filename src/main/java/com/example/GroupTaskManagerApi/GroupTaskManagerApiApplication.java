c0b9cb97c8b5db54a40565c1b2179760e18abfeb
m-mika-99 <sysevo.mikami@gmail.com>
2026-02-10 20:48:54 +0900

    SpringInitializrで作成

---
+++ src/main/java/com/example/GroupTaskManagerApi/GroupTaskManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.GroupTaskManagerApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupTaskManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupTaskManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GroupTaskManagerApi/GroupTaskManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GroupTaskManagerApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupTaskManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
